8dc6f052cb1f65f5714ea95625ac9d00f0ba495e
andrew <sandbbk@gmail.com>
2018-12-16 20:41:54 +0200

    Chsnge debugging to false

--- employees/settings.py
+++ employees/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&20d$-r-mos3ggcin$x%uk1_qv4$84a-(5pwqvg0gakq$^-!-o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
